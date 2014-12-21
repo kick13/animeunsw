f4f2771fbfecbf0ddf85dd5361a027f9348d7684
Richard Wang <zhwang@live.com.au>
2014-12-21 13:13:40 +1100

    Integrate homepage hero, global footer from design

--- footer.php
+++ footer.php
@@ -2,23 +2,18 @@
 
 				<div id="inner-footer" class="wrap cf">
 
-					<nav role="navigation">
-						<?php wp_nav_menu(array(
-    					'container' => '',                              // remove nav container
-    					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
-    					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-    					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
-    					'theme_location' => 'footer-links',             // where it's located in the theme
-    					'before' => '',                                 // before the menu
-        			'after' => '',                                  // after the menu
-        			'link_before' => '',                            // before each link
-        			'link_after' => '',                             // after each link
-        			'depth' => 0,                                   // limit the depth of the nav
-    					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
-						)); ?>
-					</nav>
-
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					<div class="text">
+						&copy; <?php echo date('Y'); ?> AnimeUNSW. Images are copyright their respective owners.
+						<div>自分を、世界さえも変えてしまえそうな...</div>
+					</div>
+					<div class="logos">
+						<a href="http://animesydney.org/" target="_blank">
+							<img src="<?php echo get_template_directory_uri(); ?>/library/images/animesydney-logo.png">
+						</a>
+						<a href="http://arc.unsw.edu.au/" target="_blank">
+							<img src="<?php echo get_template_directory_uri(); ?>/library/images/arc-logo.png">
+						</a>
+					</div>
 
 				</div>
 
