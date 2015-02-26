ca25246f3e48b5528abf01814cbbb233263d0b6c
Richard Wang <zhwang@live.com.au>
2015-02-26 13:47:30 +1100

    Start restyling menu to match new concept

--- header_nav_walker.php
+++ header_nav_walker.php
@@ -6,13 +6,21 @@ class AUNSWNavMenuWalker extends Walker_Nav_Menu {
     // Creates list element and link
     parent::start_el($temp, $item, $depth, $args);
 
+    // CSS class-ify the post title
+    $title_sanitized = preg_replace('/[^A-Za-z]/', '', $item->title);
+    $title_sanitized = strtolower($title_sanitized);
+
     // Add icon before link text, wrap link text
     $temp = preg_replace(
       '/(<a .*?>)(.*?)(<\/a>)/i',
       '${1}
-        <span class="icon iconic star"></span>
-        <span class="text">${2}</span>
-        <span class="text-subtitle"></span>
+        <span class="item-content ' . $title_sanitized . '">
+          <span class="icon"></span>
+          <span class="text-wrap">
+            <span class="text">${2}</span>
+            <span class="subtitle"></span>
+          </span>
+        </span>
        ${3}', $temp);
 
     // Output the result
