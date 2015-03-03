d803982fd9ed59b6a34a3248b333e40f5213463a
Richard Wang <zhwang@live.com.au>
2015-03-03 17:16:54 +1100

    Start updating footer to match new mockup

--- footer.php
+++ footer.php
@@ -1,20 +1,33 @@
-			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+			<footer role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
-				<div id="inner-footer" class="wrap cf">
-
-					<div class="text">
-						&copy; <?php echo date('Y'); ?> AnimeUNSW. Images are copyright their respective owners.
-						<div>自分を、世界さえも変えてしまえそうな...</div>
+				<div class="footer uwotadblock">
+					<div class="wrap cf">
+						<h2>We &lt;3 our sponsors</h2>
 					</div>
-					<div class="logos">
-						<a href="http://animesydney.org/" target="_blank">
-							<img src="<?php echo get_template_directory_uri(); ?>/library/images/animesydney-logo.png">
-						</a>
-						<a href="http://arc.unsw.edu.au/" target="_blank">
-							<img src="<?php echo get_template_directory_uri(); ?>/library/images/arc-logo.png">
-						</a>
-					</div>
+				</div>
 
+				<div class="footer main">
+					<div id="inner-footer" class="wrap cf">
+						<h2>Obligatory bottom of website</h2>
+
+						<div class="text">
+							&copy; <?php echo date('Y'); ?> AnimeUNSW. Images are copyright their respective owners.
+							<div>Design MURAMIDOKI created with &lt;3 by your 2015 Media Director.</div>
+						</div>
+
+						<div class="logos">
+							<a href="http://animesydney.org/" target="_blank">
+								<img src="<?php echo get_template_directory_uri(); ?>/library/images/animesydney-logo.png">
+							</a>
+							<a href="http://arc.unsw.edu.au/" target="_blank">
+								<img src="<?php echo get_template_directory_uri(); ?>/library/images/arc-logo.png">
+							</a>
+						</div>
+
+						<aside class="background-text">
+							自分を、世界さえも変えてしまえそうな...
+						</aside>
+					</div>
 				</div>
 
 			</footer>
@@ -26,4 +39,4 @@
 
 	</body>
 
-</html> <!-- end of site. what a ride! -->
+</html>
